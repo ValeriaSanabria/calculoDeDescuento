7da02dac8aa8c0ce790bf23911bfbf12a04ab412
Valeria Sanabria <103613127+ValeriaSanabria@users.noreply.github.com>
2022-05-02 12:26:45 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+rotulo1.innerHTML = "Inserte precio: ";
+let precioProducto: number = 450.5;
+let porcentajeDescuento: number = 0.1;
+let descuento: number = precioProducto * porcentajeDescuento;
+//let precioFinal: number = precioProducto - descuento;
+let precioProducto = document.getElementById("Precio");
+console.log(precioFinal);
+
+btnEnv.addEventListener("click", () => {
+  let precioFinal: number = Number(base.value) * Number(altura.value);
+  console.log("Resultado del área:", area);
+});
